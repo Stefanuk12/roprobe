a39325426c39f075ec33eb8e3145f5f458ff2a44
Stefan Petrovic <stefan@petrovic.foo>
2026-06-19 20:55:49 +0100

    chore: migrate template

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Luau Roblox exploit script dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pesde.url = "github:lukadev-0/pesde-nix";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      pesde,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+        toml = builtins.fromTOML (builtins.readFile ./client/pesde.toml);
+        name = builtins.elemAt (lib.splitString "/" toml.name) 1;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          inherit name;
+
+          packages = [
+            pesde.packages.${system}.default
+
+            pkgs.mise
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.gcc
+            pkgs.openssl
+            pkgs.pkg-config
+          ];
+
+          shellHook = ''
+            export PATH="$HOME/.cargo/bin:$PATH"
+            eval "$(mise activate bash)"
+            # The pesde project (and its mise.toml) lives in client/
+            (cd client && mise install)
+
+            echo ""
+            echo "🌙 dev shell ready"
+            echo "   Tools managed by mise (config in client/) - 'cd client' then 'mise ls'"
+            echo "   Run 'cd client && mise t' to see available tasks"
+          '';
+        };
+      }
+    );
+}
